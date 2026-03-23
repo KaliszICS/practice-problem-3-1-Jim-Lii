c2f06a62e0de0b5a96c5d0e89c44b0d90f5f27eb
Jim-Lii <343347795@tdsb.ca>
2026-03-23 21:00:02 +0000

    jhdbhjfs

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,7 +1,43 @@
+/*
+File Name: If Statements
+Author: Jim Li
+Date Created: Mar. 23, 2026
+Date Last Modified: Mar. 23, 2026
+*/
+
 public class PracticeProblem {
 
 	public static void main(String args[]) {
 
 	}
 
+	public static String evenOrOdd(int num){
+	String evenOrOdd = "placeholder";
+	if (num % 2 == 0){
+	evenOrOdd = "Even";
+	} else {
+	evenOrOdd = "Odd";
+	}
+	return evenOrOdd;
+	}
+
+	public static String teacherOrStudent(String name){
+	String teacherOrStudent = "placeholder";
+	if(name.equals("Kalisz")){
+		teacherOrStudent = "Teacher";
+	} else {
+		teacherOrStudent = "Student";
+	}
+	return teacherOrStudent;
+	}
+
+	public static int fartherFromZero(int num){
+	if (num < 0){
+		num -= 5;
+	}
+	if (num > 0){
+		num += 5;
+	}
+	return num;
+	}
 }
